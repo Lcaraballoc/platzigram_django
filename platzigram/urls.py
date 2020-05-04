9c28d5e37b376c8aab52e79c2135b5a1f82ea922
Luis Caraballo <luisred61@gmail.com>
2020-05-03 20:58:34 -0500

    Admin site configured for Users and Profile

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,3 +1,5 @@
+#Django
+from django.contrib import admin
 from django.urls import path
 
 from platzigram import views as local_views
@@ -5,6 +7,8 @@ from posts import views as posts_views
 
 
 urlpatterns = [
+     path('admin/', admin.site.urls),
+    
     path('hello-world/', local_views.hello_word ),
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>', local_views.say_hi),
